eaff103af51089d358d5431ab3ffb62e69c2173c
chaotic-enigma <loopholesameer@gmail.com>
2018-08-13 13:10:21 +0530

    added key points

--- daily_planner.py
+++ daily_planner.py
@@ -4,6 +4,7 @@ import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
+from datetime import datetime as dtt
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
@@ -13,30 +14,55 @@ today_tasks = 15
 
 specific_day = {
 	'Tasks' : [None] * today_tasks,
-	'Time Estimated (hrs)' : [None] * today_tasks,
-	'Time Taken (hrs)' : [None] * today_tasks 
+	'Guess Time' : [None] * today_tasks,
+	'Time Taken' : [None] * today_tasks 
 }
 
 specific_day = pd.DataFrame(specific_day)
-specific_day = specific_day[['Tasks', 'Time Estimated (hrs)', 'Time Taken (hrs)']]
+specific_day = specific_day[['Tasks', 'Guess Time', 'Time Taken']]
 
 app.layout = html.Div([
 	html.Div([
 		html.Div([
-			html.H4('Daily Progress Tracker', style={'textAlign' : 'center'}),
-			dcc.Location(id='location', refresh=False),
-			html.Div(id='output'),
-			dt.DataTable(
-				id='daily-table',
-				rows=specific_day.to_dict('records'),
-				columns=specific_day.columns,
-				editable=True
-			)
-		], className='five columns', style={'textAlign' : 'center'}),
+			html.Div([
+				html.Div([
+					html.H4('Daily Progress Tracker', style={'textAlign' : 'center'}),
+					dcc.Location(id='location', refresh=False),
+					html.Div(id='output'),
+					dt.DataTable(
+						id='daily-table',
+						rows=specific_day.to_dict('records'),
+						columns=specific_day.columns,
+						editable=True
+					)
+				], className='four columns', style={'textAlign' : 'center'}),
+				html.Div([
+					html.H4('Progress Graph', style={'textAlign' : 'center'}),
+					dcc.Graph(id='daily-progress'),
+				], className='eight columns')
+			], className='row'),
+		], className='nine columns'),
 		html.Div([
-			html.H4('Progress Graph', style={'textAlign' : 'center'}),
-			dcc.Graph(id='daily-progress'),
-		], className='seven columns')
+			html.Div(html.H3('Key points'), 
+		 		style={'textAlign' : 'center'}),
+			html.Hr(),
+			html.Div([
+				dcc.DatePickerSingle(
+					id='date-picker-single',
+					date=dtt.now()			
+				)
+			], style={'margin-bottom' : 30}),
+			html.Div([
+				dcc.Textarea(
+					placeholder='Key points',
+					value='',
+					rows=50,
+					style={'width' : '100%'},
+					wrap=True,
+					lang='en'
+				)
+			]),
+		], className='three columns', style={'textAlign' : 'center'}),
 	], className='row'),
 	html.Div([
 		dcc.Textarea(
@@ -58,8 +84,8 @@ def track_progress(rows):
 	ddf = pd.DataFrame(rows)
 	return {
 		'data' : [
-			{'x' : ddf['Tasks'], 'y' : ddf['Time Estimated (hrs)'], 'name' : 'Estimated'},
-			{'x' : ddf['Tasks'], 'y' : ddf['Time Taken (hrs)'], 'name' : 'Taken'}
+			{'x' : ddf['Tasks'], 'y' : ddf['Guess Time'], 'name' : 'Estimated'},
+			{'x' : ddf['Tasks'], 'y' : ddf['Time Taken'], 'name' : 'Taken'}
 		]
 	}
 
